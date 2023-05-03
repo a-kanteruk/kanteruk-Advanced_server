2626472e88907b80487555afb3e5dad2562d7de2
a-kanteruk <a.kanteruk@tg.dunice.net>
2023-05-03 17:24:39 +0300

    Initial repository.

---
+++ src/test/java/net/dunice/newsFeed/NewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dunice.newsFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
