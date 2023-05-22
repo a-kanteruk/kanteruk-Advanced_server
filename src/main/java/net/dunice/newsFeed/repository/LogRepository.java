28631b4d11887e4a716ac33aebe70257bd24ed3a
a-kanteruk <a.kanteruk@tg.dunice.net>
2023-05-22 15:12:06 +0300

    feature/#12-Create a logger function. Code fixed according to CheckStyle.

---
+++ src/main/java/net/dunice/newsFeed/repository/LogRepository.java
@@ -0,0 +1,9 @@
+package net.dunice.newsFeed.repository;
+
+import net.dunice.newsFeed.models.LogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogRepository extends JpaRepository<LogEntity, Long> {
+}
